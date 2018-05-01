8b5ee5a7c4f3c5ffda7a2a6385789a86b66a62f3
nikolay.puzyrev <nikolay.puzyrev@team.wrike.com>
2018-05-01 17:31:10 +0300

    created testTemplateMethod

--- main.py
+++ main.py
@@ -5,10 +5,14 @@ class TestCase:
     def setUp(self):
         pass
 
-    def run(self):
+    def run(self, result):
+        result.testStarted()
         self.setUp()
-        method = getattr(self, self.name)
-        method()
+        exec "self." + self.name + "()"
+        self.tearDown()
+
+    def tearDown(self):
+        pass
 
 
 class WasRun(TestCase):
@@ -16,21 +20,17 @@ class WasRun(TestCase):
         TestCase.__init__(self, name)
 
     def setUp(self):
-        self.wasRun = None
-        self.wasSetUp = 1
+        self.log = "setUp "
 
     def testMethod(self):
-        pass
+        self.log = self.log + "testMethod "
+
+    def tearDown(self):
+        self.log = self.log + "tearDown "
 
 
 class TestCaseTest(TestCase):
-    def setUp(self):
-        self.test = WasRun("testMethod")
-
-    def testRunning(self):
-        self.test.run()
-        assert(self.test.wasRun)
-
-    def testSetUp(self):
-        self.test.run()
-        assert(self.test.wasSetUp)
+    def testTemplateMethod(self):
+        test = WasRun("testMethod")
+        test.run()
+        assert("setUp testMethod tearDown " == test.log)
