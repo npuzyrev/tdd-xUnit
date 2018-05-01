04731a1d27e05c7a9a66bbf031b5bc9a2ff58a4a
npuzyrev <na@pzrv.ru>
2018-05-01 15:16:50 +0300

    added method run

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 class WasRun:
     def __init__(self, name):
         self.wasRun = None
+        self.name = name
+
+    def run(self):
+        method = getattr(self, self.name)
+        method()
 
     def testMethod(self):
-        self.wasRun = 1
+        pass
 
 
 
 test = WasRun("testMethod")
 print(test.wasRun)
-test.testMethod()
+test.run()
 print(test.wasRun)
