4477c57d8cc52be547667abffcffb40ac67b1645
npuzyrev <na@pzrv.ru>
2018-05-01 15:29:18 +0300

    added superclass TestCase

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
-class WasRun:
+class TestCase:
     def __init__(self, name):
-        self.wasRun = None
         self.name = name
 
     def run(self):
         method = getattr(self, self.name)
         method()
 
+
+
+class WasRun(TestCase):
+    def __init__(self, name):
+        self.wasRun = None
+        TestCase.__init__(self, name)
+
     def testMethod(self):
         pass
-
-
-
-test = WasRun("testMethod")
-print(test.wasRun)
-test.run()
-print(test.wasRun)
